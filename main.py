69cb1ccdbd80525e34f54da346345a09695cffd3
Lasha Kamadadze <kamadadze833@gmail.com>
2024-10-15 20:46:52 +0400

    Version 2.0

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Gregory')
+    name = input("Inser Name: ")
+    print_hi(name)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
